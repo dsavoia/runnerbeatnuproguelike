a3f2e15d3be75e495970c002d02395032f4ec221
Daniel Savoia <Daniel Savoia>
2016-12-19 02:53:41 -0200

    Starting Basics Enemies

--- RunnerBeatUpRogueLike/Assets/Scripts/Player/PlayerHUD.cs
+++ RunnerBeatUpRogueLike/Assets/Scripts/Player/PlayerHUD.cs
@@ -16,6 +16,6 @@ public class PlayerHUD : MonoBehaviour {
 	void Update ()
     {
         distanceText.text = playerInfo.distanceWalked.ToString("F0") + " meters";
-        goldAmount.text = playerInfo.goldEarned.ToString() + " gold";
+        goldAmount.text = playerInfo.goldEarned.ToString() + " g";
     }
 }
